720d6cce69aad66e875ed476439b789c2d0443b9
DanielaPerez <daniela.perez@soyrosarista.edu.co>
2024-07-23 15:10:44 -0500

    creando servicios

--- app.py
+++ app.py
@@ -6,4 +6,13 @@ from routes.rutasAPI import rutas
 
 app=FastAPI()
 
+# Habilitar CORS para permitir solicitudes desde todos los orígenes
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite solicitudes desde cualquier origen. Ajusta según sea necesario.
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos los métodos HTTP. Ajusta según sea necesario.
+    allow_headers=["*"],  # Permite todas las cabeceras. Ajusta según sea necesario.
+)
+
 app.include_router(rutas)
